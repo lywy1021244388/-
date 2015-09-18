642fa726d0a9806a9e0a650d33a3d26479fb3390
yang <lywy1021244388@163.com>
2015-09-18 11:27:05 +0800

    Initial Commit

---
+++ 倒计时/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  倒计时
+//
+//  Created by yan on 15/9/18.
+//  Copyright (c) 2015年 yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 倒计时/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  倒计时
+//
+//  Created by yan on 15/9/18.
+//  Copyright (c) 2015年 yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
